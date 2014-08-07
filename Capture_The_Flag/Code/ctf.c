98a49b0c49739677546df8633b2102ac17393d0c
rnmaggott <rnmaggott@gmail.com>
2014-08-08 00:06:25 +0200

    Got timers for both teams working depending on the button pressed. Need to now develop and build circuitry.

--- Capture_The_Flag/Code/ctf.c
+++ Capture_The_Flag/Code/ctf.c
@@ -31,18 +31,92 @@ void initializeInt();
 void initializeTimer1();
 void initializeTimer0();
 
+#define sf_DEBUG   PORTBbits.RB5
+#define sf_TEAM1o  PORTBbits.RB0
+#define sf_TEAM2o  PORTBbits.RB1
+#define sf_TEAM1i  PORTBbits.RB2 //RED
+#define sf_TEAM2i  PORTBbits.RB3 //BLUE
+
+int tflag = 0;
+
 void main(void) {
 
+    int counter = 0;
+    int team1 = 0;
+    int team2 = 0;
+    int team1count = 0;
+    int team2count = 0;
+    int t1counter = 0;
+    int t2counter = 0;
+    int fivef = 0;
+    int fivemin = 0;
+
+    sf_TEAM1o = 0;
+    sf_TEAM2o = 0;
+
+    startUp();
     for(;;){
-        
+        if(tflag){
+            tflag = 0;
+            //sf_DEBUG = ~sf_DEBUG;
+            if(team1count){
+                t1counter++;
+            }
+            if(team2count){
+                t2counter++;
+            }
+            if(fivef){
+                fivemin++;
+            }
+        }
+        if(t1counter >= 2451)
+        {
+            t1counter = 0;
+            sf_TEAM1o = 1;
+            sf_TEAM2o = 0;
+            team1 = 1;
+            team2 = 0;
+            fivef = 1;
+            fivemin = 0;
+        }
+        if(t2counter >= 2451)
+        {
+            t2counter = 0;
+            sf_TEAM1o = 0;
+            sf_TEAM2o = 1;
+            team1 = 0;
+            team2 = 1;
+            fivef = 1;
+            fivemin = 0;
+        }
+        if(fivemin >= 4902){
+            sf_DEBUG = 1;
+        }
+        //TEAM1
+        if(sf_TEAM1i){
+            team1count = 1;
+
+        }
+        else{
+            team1count = 0;
+            t1counter = 0;
+        }
+        //TEAM2
+        if(sf_TEAM2i){
+            team2count = 1;
+        }
+        else{
+            team2count = 0;
+            t2counter = 0;
+        }
+
     }
-    return;
 }
 
 void startUp(){
     OSCCON = 0x70;
 
-    TRISB = 0b0001111;
+    TRISB = 0b1011100;
     PORTB = 0;
 
     initializeInt();
@@ -59,16 +133,14 @@ void interrupt isr(void){
 	//Timer1 Interrupt
     if(TMR1IF){
         TMR1IF = 0; 			// clear interrupt flag
-        if (tflag){
-            
-        }
-        else{
-            
-        }
+        //tflag = 1;
+        //sf_DEBUG = ~sf_DEBUG;
     }
     //timer0 interrupt
     if(TMR0IF){
-        
+        TMR0IF = 0;
+        TMR0 = 0;
+        tflag = 1; //4ms flag
     }
 }
 
@@ -93,3 +165,5 @@ void initializeTimer0(){
     OPTION_REGbits.PSA = 0;     //Prescaler on timer0
     OPTION_REGbits.PS = 0b100;	//set prescaler value (1:32)
 }
+
+//Time on = 1/(Fosc/4)*32*255*2
